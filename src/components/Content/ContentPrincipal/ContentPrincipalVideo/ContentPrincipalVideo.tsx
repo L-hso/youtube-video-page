804c13d9a68050ae907fa605e6fd44508b026ff3
L-hso <lhsoliveira2008@hotmail.com>
2024-03-18 18:00:40 -0300

    Projeto completo

--- src/components/Content/ContentPrincipal/ContentPrincipalVideo/ContentPrincipalVideo.tsx
+++ src/components/Content/ContentPrincipal/ContentPrincipalVideo/ContentPrincipalVideo.tsx
@@ -1,39 +1,10 @@
-import { useRef, useState } from 'react';
-import {VideoBar} from './VideoBar';
-
-export function ContentPrincipalVideo() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  const [actualTime, setActualTime] = useState(0);
-  function durationHandler(e:HTMLVideoElement){
-    console.log(e.duration);
-    return e.duration;
-  }
-
-  let duration;
-    
-  function currentTimeHandler (e: HTMLVideoElement) {
-    
-
-  }
-  
+export function ContentPrincipalVideo() {  
   return (
+  
     <div className="group relative rounded-md overflow-hidden flex justify-center cursor-pointer"
-    onClick={()=>videoRef.current!.paused?videoRef.current!.play():videoRef.current!.pause()}>
-      <video
-        src="src\assets\Não fui feito para Programar != Bloqueado no Código.mp4"
-        ref={videoRef}
-        onLoadedMetadata={(e)=>{duration = durationHandler(e.currentTarget)}}
-        controls
-        className="-z-10"
-        id="video"
-        
-        onTimeUpdate={(e)=>{console.log(e.currentTarget.currentTime);}}
-        disablePictureInPicture
-        
-      />
-      <VideoBar actualTime={actualTime} totalTime={duration}/>
-      <div className="absolute bottom-0 w-full h-0 transition-all duration-100 ease-in-out invisible bg-gradient-to-b from-transparent from-65% to-black group-hover:h-full group-hover:visible"/>
+    >
+    <iframe height="500" className="w-full"
+ src="https://www.youtube.com/embed/ZeNyjnneq_w?si=5f8jrfrJ79T1GzdH" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   );
 }
--- src/components/Content/ContentPrincipal/ContentPrincipalVideo/VideoToolbar.tsx
+++
@@ -1,7 +0,0 @@
-export function VideoToolbar(){
-  return (
-    <menu>
-      <li></li> 
-    </menu>
-  );
-}
